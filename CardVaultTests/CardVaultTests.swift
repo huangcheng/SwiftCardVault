4ce57a576ab221e1ca1caf0dd6ef8d4dc8c6071b
HUANG Cheng <cheng@duck.com>
2026-03-26 10:30:30 +0800

    Initial Commit

---
+++ CardVaultTests/CardVaultTests.swift
@@ -0,0 +1,16 @@
+//
+//  CardVaultTests.swift
+//  CardVaultTests
+//
+//  Created by Cheng Huang on 2026-03-26.
+//
+
+import Testing
+
+struct CardVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
